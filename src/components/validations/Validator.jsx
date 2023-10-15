238a55d39c68f602b765671a41b061f664549f76
NRDobariya <65714240+NRDobariya@users.noreply.github.com>
2023-10-15 09:56:40 +0300

    fix

--- src/components/validations/Validator.jsx
+++ src/components/validations/Validator.jsx
@@ -3,7 +3,17 @@ import React, { createContext, useEffect, useState } from "react";
 import Validator from "validatorjs";
 import { HELPER } from "./../../services";
 
-export const ValidationContext = createContext(null);
+function formatBytes(bytes, decimals = 2) {
+  if (!+bytes) return '0 Bytes'
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
 
 const _registerConfirmPasswordValidations = (password) => {
   Validator.register(
